335264ebf9f955daac0eb2b1d80b55fc9c6adc49
Idan Izhaki <izhaki.idan@gmail.com>
2018-09-17 13:27:02 +0300

    Basic scraping to csv & csv to html option

---
+++ scrape.py
@@ -0,0 +1,45 @@
+import requests as req
+from bs4 import BeautifulSoup
+from csv import writer, reader
+
+
+def init():
+    user_input = input("Paste in a full url (including http/s): ")
+    scrape_site(user_input)
+    activae_gen = input("Generate CSV to HTML? (y/n): ")
+    if activae_gen[0] == 'y':
+        gen_html(user_input)
+
+
+def scrape_site(url):
+    incoming = req.get(url)
+    print(f"Got status response: {incoming.status_code}")
+    if incoming.status_code == 200:
+        name = url.split('.')[1]
+        with open(f'./csv/{name}.csv', 'w', encoding="utf-8") as csv_file:
+            csv_writer = writer(csv_file)
+            csv_writer.writerow(["Index", "Url", "Alt"])
+            soup = BeautifulSoup(incoming.content, "html.parser")
+            img_tags = soup.find_all("img", {"src": True})
+            for i, img in enumerate(img_tags):
+                alt = img["alt"]
+                src = img["src"]
+                csv_writer.writerow([i, src, alt])
+        print(f"Done scraping, closing {name}.csv file")
+
+
+def gen_html(url):
+    name = url.split('.')[1]
+    with open(f'./csv/{name}.csv', 'r', encoding="utf-8") as csv_file:
+        csv_reader = reader(csv_file)
+        items = list(csv_reader)
+        with open(f'./html/{name}.html', 'w') as html_file:
+            html_file.write("<body>\n")
+            for item in items:
+                if item:
+                    html_file.write(f"<img src={item[1]} alt={item[2]}>\n")
+            html_file.write("</body>")
+        print(f"Done, {name}.html was created")
+
+
+init()
