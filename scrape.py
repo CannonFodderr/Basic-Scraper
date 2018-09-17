1a52ba39be20bf937b57810014c9db1c496f91a2
Idan Izhaki <izhaki.idan@gmail.com>
2018-09-17 16:32:40 +0300

    Change HTML generation from list instead of csv file

--- scrape.py
+++ scrape.py
@@ -1,41 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 from csv import writer, reader
+STATUS_SUCCESS = 200
+FILES_ENCODING = "utf-8"
 
-
-def init():
-    """Main init function runs on startup"""
+def main():
+    """Main function runs on startup"""
     choosen_url = input("Paste in a full url (including http/s): ")
-    site_data = scan_site_images(choosen_url)
-    name = name_gen(choosen_url)
-    write_csv(site_data, name)
-    activae_gen = input("Generate CSV to HTML? (y/n): ")
-    if activae_gen[0] == 'y':
-        gen_html(choosen_url, name)
+    site_data = request_site(choosen_url)
+    domain_name = name_gen(choosen_url)
+    images_list = write_csv(site_data, domain_name)
+    activate_gen = input("Generate images list to HTML? (y/n): ")
+    if activate_gen[0] == 'y':
+        gen_html(domain_name, images_list)
 
 
 def name_gen(url_string):
     """Return a website name"""
-    name = url_string.split('/')[2]
-    return name
+    domain_name = url_string.split('/')[2]
+    return domain_name
 
 
-def write_csv(url_data, name):
+def write_csv(url_data, domain_name):
     """parses the data back a .CSV file"""
-    with open(f'./csv/{name}.csv', 'w', encoding="utf-8") as csv_file:
+    with open(f'./csv/{domain_name}.csv', 'w', encoding=FILES_ENCODING) as csv_file:
         csv_writer = writer(csv_file)
         csv_writer.writerow(["Index", "Url"])
         soup = BeautifulSoup(url_data.content, "html.parser")
         img_tags = soup.find_all("img", {"src": True})
+        images_list = []
         for i, img in enumerate(img_tags):
-            src = img["src"]
-            csv_writer.writerow([i, src])
-        print(f"Done scraping, closing {name}.csv file")
+            row_data = i, img["src"]
+            csv_writer.writerow([row_data])
+            images_list.append(row_data)
+        print(f"Done scraping, closing {domain_name}.csv file")
+        return images_list
 
 
-def scan_site_images(url):
+def request_site(url):
     """Scraper sends a request to site"""
-    STATUS_SUCCESS = 200
     get_result = requests.get(url)
     print(f"Got status response: {get_result.status_code}")
     if get_result.status_code is not STATUS_SUCCESS:
@@ -43,16 +46,13 @@ def scan_site_images(url):
     return get_result
 
 
-def gen_html(url, name):
-    """Optional function turns the .CSV file into an .HTML file"""
-    with open(f'./csv/{name}.csv', 'r', encoding="utf-8") as csv_file:
-        csv_reader = reader(csv_file)
-        items = list(csv_reader)
-        with open(f'./html/{name}.html', 'w') as html_file:
-            html_file.write("<body>\n")
-            [html_file.write(f"<img src={item[1]}>\n") for item in items if item]
-            html_file.write("</body>")
-        print(f"Done, {name}.html was created")
+def gen_html(domain_name, images_list):
+    """Optional function turns the images_list file into an .HTML file"""
+    with open(f'./html/{domain_name}.html', 'w') as html_file:
+        html_file.write("<body>\n")
+        [html_file.write(f"<img src={item[1]}>\n") for item in images_list if len(item) > 1]
+        html_file.write("</body>")
+    print(f"Done, {domain_name}.html was created")
 
 
-init()
+main()
